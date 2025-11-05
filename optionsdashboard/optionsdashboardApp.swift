c7b47f03b7bed2dbf8ab272b3d670244b3f17911
coldhands175 <msbaxter@gmail.com>
2025-11-05 14:19:38 -0800

    Initial Commit

---
+++ optionsdashboard/optionsdashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  optionsdashboardApp.swift
+//  optionsdashboard
+//
+//  Created by Michael Baxter on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct optionsdashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ optionsdashboardTests/optionsdashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  optionsdashboardTests.swift
+//  optionsdashboardTests
+//
+//  Created by Michael Baxter on 2025-11-05.
+//
+
+import Testing
+@testable import optionsdashboard
+
+struct optionsdashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
